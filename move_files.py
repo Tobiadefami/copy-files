0310cba3ccbfe45271a4dcbe3fa4573f592862d4
tobiadefami <tobiadefami@gmail.com>
2022-09-05 12:24:37 +0100

    first commit

---
+++ move_files.py
@@ -0,0 +1,24 @@
+import glob
+import fire
+import os
+import tqdm
+import shutil
+
+
+def ordering_files(file_path:str, new_file_path:str):
+
+    fns = list(glob.glob(file_path, recursive=True))
+    for file in tqdm.tqdm(fns):
+        new_path = os.path.join(os.path.dirname(__file__), f'{new_file_path}')
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)  
+        shutil.copy(file, new_path) 
+
+
+
+def main(src, dst):
+    return ordering_files(src, dst)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
